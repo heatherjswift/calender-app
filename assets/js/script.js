33429c1cc009e42061b3af8f0d6604f65c09a69c
Hede <Heatherj.swift@gmail.com>
2021-04-09 14:58:49 -0400

    each section saves and is uniform, get current hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,15 @@ $(".btn").on("click", function() {
 })
 
 $("#9").val(localStorage.getItem("9"))
+$("#10").val(localStorage.getItem("10"))
+$("#11").val(localStorage.getItem("11"))
+$("#12").val(localStorage.getItem("12"))
+$("#13").val(localStorage.getItem("13"))
+$("#14").val(localStorage.getItem("14"))
+$("#15").val(localStorage.getItem("15"))
+$("#16").val(localStorage.getItem("16"))
+$("#17").val(localStorage.getItem("17"))
+
+var currentTime = moment().format("HH");
+
+console.log(currentTime)
